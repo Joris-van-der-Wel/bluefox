9594c735ea929d96ce82c1e098a4aeb0065bd2b0
Joris van der Wel <joris@jorisvanderwel.com>
2017-06-27 11:26:22 +0200

    Send a periodic log message during integration tests with karma
    The karma CLI stops the test if no message has been received for more than 10 seconds, even though a test is still running.

--- test/benchmark/environments/karma.js
+++ test/benchmark/environments/karma.js
@@ -1,4 +1,6 @@
 'use strict';
 /* eslint import/no-unassigned-import:off */
+console.log('karma keep alive');
+setInterval(() => console.log('karma keep alive'), 2000);
 require('../../karmaTestEnv');
 require('../manifest');
--- test/benchmark/manifest.js
+++ test/benchmark/manifest.js
@@ -1,6 +1,5 @@
 'use strict';
 
-setInterval(() => console.log('keep alive'), 2000);
 console.info(
     '!BENCH!',
     'Test Name',
