5ff5e02e687e2b3b0b74541e201515aecc8857de
Joris van der Wel <joris@jorisvanderwel.com>
2018-02-28 10:48:27 +0100

    Travis CI: Use the default firefox from ubuntu trusty
    The alternative apt repository is no longer needed, which was used because the firefox version on trusty was very old, but this is no longer the case.

--- .travis/install_browsers.sh
+++ .travis/install_browsers.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 lsb_release -a
-sudo add-apt-repository -y ppa:ubuntu-mozilla-security/ppa
 sudo apt-get -qq update
 sudo apt-get install -y xvfb openbox chromium-browser firefox
 "$FIREFOX_BIN" --version
